1dba20cfa4548ea0ecb21ddefb3b2552606b2049
Julien Dupouy <julien.dupouy.fr@gmail.com>
2013-12-09 19:56:55 +0100

    - Default value for enum field must be an integer - Check field group name format

--- model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/AbstractMedanyModelConstraint.java
+++ model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/AbstractMedanyModelConstraint.java
@@ -6,14 +6,12 @@ import org.imogene.model.core.FieldEntity;
 import org.imogene.model.core.FieldGroup;
 import org.imogene.model.core.Project;
 
-
 /**
  * 
  * @author MEDES-IMPS
- *
+ * 
  */
-public abstract class AbstractMedanyModelConstraint extends
-		AbstractModelConstraint {
+public abstract class AbstractMedanyModelConstraint extends AbstractModelConstraint {
 
 	/**
 	 * Format message with target property
@@ -22,45 +20,60 @@ public abstract class AbstractMedanyModelConstraint extends
 	 * @param entity The target entity
 	 * @return The formated message
 	 */
-	public String formatMessage(String message, FieldEntity entity){
+	public String formatMessage(String message, FieldEntity entity) {
 		String newMessage = message;
-		newMessage = newMessage.replace("%FIELD_NAME%", entity.getName());		
+		newMessage = newMessage.replace("%FIELD_NAME%", entity.getName());
 		return newMessage;
 	}
-	
-	public String formatMessage(String message, CardEntity card, FieldEntity field){
+
+	/**
+	 * Format message with target property
+	 * 
+	 * @param message The message template
+	 * @param group The target group
+	 * @return The formated message
+	 */
+	public String formatMessage(String message, FieldGroup group) {
+		String newMessage = message;
+		newMessage = newMessage.replace("%GROUP_NAME%", group.getName());
+		return newMessage;
+	}
+
+	public String formatMessage(String message, CardEntity card, FieldEntity field) {
 		String newMessage = message;
 		newMessage = newMessage.replace("%FIELD_NAME%", field.getName());
 		newMessage = newMessage.replace("%ENTITY_NAME%", card.getName());
 		return newMessage;
 	}
-	
+
 	public String formatMessage(String message, CardEntity card, FieldGroup group) {
 		String newMessage = message;
 		newMessage = newMessage.replace("%GROUP_NAME%", group.getName());
 		newMessage = newMessage.replace("%ENTITY_NAME%", card.getName());
 		return newMessage;
 	}
-	
+
 	/**
 	 * Format message with target property
+	 * 
 	 * @param message The message template
 	 * @param card The target CardEntity
 	 * @return The formated message
 	 */
-	public String formatMessage(String message, CardEntity card){
+	public String formatMessage(String message, CardEntity card) {
 		String newMessage = message;
 		newMessage = newMessage.replace("%ENTITY_NAME%", card.getName());
 		return newMessage;
 	}
-	
+
 	/**
 	 * Format message with target property
+	 * 
 	 * @param message The message template
 	 * @param card The target Project
 	 * @return The formated message
 	 */
-	public String formatMessage(String message, Project project){
+	public String formatMessage(String message, Project project) {
 		String newMessage = message;
 		newMessage = newMessage.replace("%PROJECT_NAME%", project.getName());
 		return newMessage;
--- model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/ObjectNameConstraint.java
+++ model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/ObjectNameConstraint.java
@@ -9,9 +9,9 @@ import org.eclipse.emf.validation.IValidationContext;
 import org.imogene.model.core.CardEntity;
 import org.imogene.model.core.EnumValue;
 import org.imogene.model.core.FieldEntity;
+import org.imogene.model.core.FieldGroup;
 import org.imogene.model.core.Project;
 import org.imogene.model.core.Role;
-
 
 /**
  * Check that object name match a regular expression
@@ -19,25 +19,26 @@ import org.imogene.model.core.Role;
 public class ObjectNameConstraint extends AbstractMedanyModelConstraint {
 
 	private static final String reg_ex = "^[a-zA-Z]+[a-zA-Z0-9_]*$";
-	
+
 	private static final String card_entity_reg_ex = "^[A-Z][a-z0-9][a-zA-Z0-9_]*$";
 
 	private static String EMPTY_ENTITY_NAME = "The name of the entity \"%ENTITY_NAME%\" should not be empty";
 
+	private static String EMPTY_GROUP_NAME = "The name of the field group \"%GROUP_NAME%\" should not be empty";
+
 	private static String EMPTY_FIELD_NAME = "The name of the entity \"%FIELD_NAME%\" should not be empty";
-	
-	private static String EMPTY_PROJECT_NAME = "The name of the project is not valid and should only contain "
-		+ reg_ex;
+
+	private static String EMPTY_PROJECT_NAME = "The name of the project is not valid and should only contain " + reg_ex;
 
 	private static String BAD_ENTITY_NAME = "The name of the entity \"%ENTITY_NAME%\" should contain only "
 			+ card_entity_reg_ex;
 
-	private static String BAD_FIELD_NAME = "The name of the field \"%FIELD_NAME%\" should contain only "
-			+ reg_ex;
-	
-	private static String BAD_PROJECT_NAME = "The name of the project is not valid and should only contain "
-		+ reg_ex;
-	
+	private static String BAD_GROUP_NAME = "The name of the field group \"%GROUP_NAME%\" should contain only " + reg_ex;
+
+	private static String BAD_FIELD_NAME = "The name of the field \"%FIELD_NAME%\" should contain only " + reg_ex;
+
+	private static String BAD_PROJECT_NAME = "The name of the project is not valid and should only contain " + reg_ex;
+
 	/**
 	 * Initializes me.
 	 */
@@ -48,9 +49,7 @@ public class ObjectNameConstraint extends AbstractMedanyModelConstraint {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse
-	 * .emf.validation.IValidationContext)
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse .emf.validation.IValidationContext)
 	 */
 	public IStatus validate(IValidationContext ctx) {
 		try {
@@ -58,24 +57,39 @@ public class ObjectNameConstraint extends AbstractMedanyModelConstraint {
 				String projectName = ((Project) ctx.getTarget()).getName();
 				if (projectName != null) {
 					if (projectName.isEmpty())
-						return ctx.createFailureStatus(new Object[] {EMPTY_PROJECT_NAME});
+						return ctx.createFailureStatus(new Object[] { EMPTY_PROJECT_NAME });
 					else if (!projectName.matches(reg_ex))
-						return ctx.createFailureStatus(new Object[] {BAD_PROJECT_NAME});
+						return ctx.createFailureStatus(new Object[] { BAD_PROJECT_NAME });
 				}
 			} else if (ctx.getTarget() instanceof CardEntity) {
 				String entityName = ((CardEntity) ctx.getTarget()).getName();
 				if (entityName != null)
 					if (entityName.isEmpty())
-						return ctx.createFailureStatus(new Object[] { formatMessage(EMPTY_ENTITY_NAME, (CardEntity) ctx.getTarget()) });
+						return ctx.createFailureStatus(new Object[] { formatMessage(EMPTY_ENTITY_NAME,
+								(CardEntity) ctx.getTarget()) });
 					else if (!entityName.matches(card_entity_reg_ex))
-						return ctx.createFailureStatus(new Object[] { formatMessage(BAD_ENTITY_NAME, (CardEntity) ctx.getTarget()) });
+						return ctx.createFailureStatus(new Object[] { formatMessage(BAD_ENTITY_NAME,
+								(CardEntity) ctx.getTarget()) });
+			} else if (ctx.getTarget() instanceof FieldGroup) {
+				String groupName = ((FieldGroup) ctx.getTarget()).getName();
+				if (groupName != null) {
+					if (groupName.isEmpty()) {
+						return ctx.createFailureStatus(new Object[] { formatMessage(EMPTY_GROUP_NAME,
+								(FieldGroup) ctx.getTarget()) });
+					} else if (!groupName.matches(card_entity_reg_ex)) {
+						return ctx.createFailureStatus(new Object[] { formatMessage(BAD_GROUP_NAME,
+								(FieldGroup) ctx.getTarget()) });
+					}
+				}
 			} else if (ctx.getTarget() instanceof FieldEntity) {
 				String fieldName = ((FieldEntity) ctx.getTarget()).getName();
 				if (fieldName != null)
 					if (fieldName.isEmpty())
-						return ctx.createFailureStatus(new Object[] { formatMessage(EMPTY_FIELD_NAME, (CardEntity) ctx.getTarget()) });
+						return ctx.createFailureStatus(new Object[] { formatMessage(EMPTY_FIELD_NAME,
+								(CardEntity) ctx.getTarget()) });
 					else if (!fieldName.matches(reg_ex))
-						return ctx.createFailureStatus(new Object[] { formatMessage(BAD_FIELD_NAME, (FieldEntity) ctx.getTarget()) });
+						return ctx.createFailureStatus(new Object[] { formatMessage(BAD_FIELD_NAME,
+								(FieldEntity) ctx.getTarget()) });
 			} else if (ctx.getTarget() instanceof EnumValue) {
 				EnumValue enumValue = (EnumValue) ctx.getTarget();
 				if (enumValue.getName() != null)
@@ -96,13 +110,11 @@ public class ObjectNameConstraint extends AbstractMedanyModelConstraint {
 
 	/** get error message for the EnumValue validation */
 	private String getErrorMessageEnumValue(String enumName) {
-		return new String("The name of the name enum value \"" + enumName
-				+ "\" should contains only " + reg_ex + ".");
+		return new String("The name of the name enum value \"" + enumName + "\" should contains only " + reg_ex + ".");
 	}
 
 	/** get error message for role */
 	private String getErrorMessageRole(String roleName) {
-		return new String("the name of the role \"" + roleName
-				+ "\" should contains only " + reg_ex + ".");
+		return new String("the name of the role \"" + roleName + "\" should contains only " + reg_ex + ".");
 	}
 }
