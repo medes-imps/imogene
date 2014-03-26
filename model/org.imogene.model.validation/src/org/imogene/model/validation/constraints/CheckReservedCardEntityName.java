88cd01ff6ba480dc1fe21db2948a79b99886dacb
Julien Dupouy <julien.dupouy.fr@gmail.com>
2014-03-26 15:48:55 +0100

    - add some reserved words

--- model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/CheckReservedCardEntityName.java
+++ model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/CheckReservedCardEntityName.java
@@ -4,39 +4,41 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.validation.IValidationContext;
 import org.imogene.model.core.CardEntity;
 
-
 /**
- *  Check that entity name don't use reserved words
+ * Check that entity name don't use reserved words
+ * 
  * @author MEDES-IMPS
  */
 public class CheckReservedCardEntityName extends AbstractMedanyModelConstraint {
 
-	private String[] reservedNames = new String[]{"Alert", "Role", "DefaultUser", "Activity", "Entity", "History"};
-	
+	private String[] reservedNames = new String[] { "alert", "role", "defaultUser", "activity", "entity", "history",
+			"case", "user" };
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse.emf.validation.IValidationContext)
 	 */
 	public IStatus validate(IValidationContext ctx) {
-		
-		try{
-			if(ctx.getTarget() instanceof CardEntity){	
-				CardEntity card = (CardEntity)ctx.getTarget();
-				for(int i=0; i<reservedNames.length; i++)
-					if(card.getName() != null && reservedNames[i].toLowerCase().equals(card.getName().toLowerCase()))
-						return ctx.createFailureStatus(new Object[]{getErrorMessage(card.getName())});
+
+		try {
+			if (ctx.getTarget() instanceof CardEntity) {
+				CardEntity card = (CardEntity) ctx.getTarget();
+				for (int i = 0; i < reservedNames.length; i++)
+					if (card.getName() != null && reservedNames[i].equals(card.getName().toLowerCase()))
+						return ctx.createFailureStatus(new Object[] { getErrorMessage(card.getName()) });
 			}
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return ctx.createSuccessStatus();
 		}
-		
+
 		return ctx.createSuccessStatus();
 	}
-	
+
 	/** create the error message */
-	private String getErrorMessage(String cardName){
-		return new String("The card entity name \""+cardName+"\" is a resrved word, please choose another.");
+	private String getErrorMessage(String cardName) {
+		return new String("The card entity name \"" + cardName + "\" is a resrved word, please choose another.");
 	}
 
 }
--- model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/CheckReservedFieldEntityName.java
+++ model/org.imogene.model.validation/src/org/imogene/model/validation/constraints/CheckReservedFieldEntityName.java
@@ -4,7 +4,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.validation.IValidationContext;
 import org.imogene.model.core.FieldEntity;
 
-
 /**
  * Check that property names don't use reserved word.
  * 
@@ -12,21 +11,16 @@ import org.imogene.model.core.FieldEntity;
  */
 public class CheckReservedFieldEntityName extends AbstractMedanyModelConstraint {
 
-	private String[] reservedNames = new String[] { "id", "Creator",
-			"CreationDate", "LastModificationDate", "Latitude", "Longitude",
-			"FieldComments", "FieldComment", "DisplayValue", "Login",
-			"Password", "NotificationLocale", "Roles", "AuthorizedRoles",
-			"NotificationMethod", "NotificationDataMethodName", "BeNotified",
-			"NotificationDelegator", "Delegate", "modified", "modifiedBy",
-			"modifiedFrom", "uploadDate", "created", "createdBy", "unread",
-			"synchronized"};
+	private String[] reservedNames = new String[] { "id", "Creator", "CreationDate", "LastModificationDate",
+			"Latitude", "Longitude", "FieldComments", "FieldComment", "DisplayValue", "Login", "Password",
+			"NotificationLocale", "Roles", "AuthorizedRoles", "NotificationMethod", "NotificationDataMethodName",
+			"BeNotified", "NotificationDelegator", "Delegate", "modified", "modifiedBy", "modifiedFrom", "uploadDate",
+			"created", "createdBy", "unread", "synchronized", "user" };
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse
-	 * .emf.validation.IValidationContext)
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse .emf.validation.IValidationContext)
 	 */
 	public IStatus validate(IValidationContext ctx) {
 		try {
@@ -34,7 +28,8 @@ public class CheckReservedFieldEntityName extends AbstractMedanyModelConstraint 
 				FieldEntity field = (FieldEntity) ctx.getTarget();
 				for (int i = 0; i < reservedNames.length; i++)
 					if (field.getName() != null && reservedNames[i].toLowerCase().equals(field.getName().toLowerCase()))
-						return ctx.createFailureStatus(new Object[] { getErrorMessage(field.getName(), field.getParentCard().getName()) });
+						return ctx.createFailureStatus(new Object[] { getErrorMessage(field.getName(), field
+								.getParentCard().getName()) });
 			}
 
 		} catch (Exception ex) {
@@ -47,9 +42,7 @@ public class CheckReservedFieldEntityName extends AbstractMedanyModelConstraint 
 
 	/** create the error message */
 	private String getErrorMessage(String fieldName, String cardName) {
-
-		return new String("The field entity name \"" + fieldName
-				+ "\" (of the card entity \"" + cardName
+		return new String("The field entity name \"" + fieldName + "\" (of the card entity \"" + cardName
 				+ "\") is a reserved word, please choose another.");
 
 	}
